2ed6e64120fa0c8af0975b3c8c037fe2a3bc857a
Revathii-Nair <revathiinair2000@gmail.com>
2026-02-06 21:32:59 +0530

    updated Parser for SELECT and condition

--- Lexer.py
+++ Lexer.py
@@ -1,4 +1,4 @@
-import Token
+from Token import *
 import re
 
 
@@ -10,34 +10,55 @@ class Lexer:
     WILDCARD_REGEX = re.compile(r"\*")
     VALUE_REGEX = re.compile(r"'[^']*'|\d+(\.\d+)?")
 
+    def split_tokens(self, text):
+        parts, i, multi_ops = [], 0, {"<=", ">=", "!="}
+        while i < len(text):
+            if i+1 < len(text) and text[i:i+2] in multi_ops:
+                parts.append(text[i:i+2]); i += 2; 
+                continue
+            if text[i] in "=<>!(),;*":
+                parts.append(text[i]); i += 1; 
+                continue
+            buf = ""
+            while i < len(text) and text[i] not in "=<>!(),;*":
+                buf += text[i]; i += 1
+            if buf: parts.append(buf)
+        return parts
+
+
     def tokenize(self, sql_text):
         tokens = []
         line = 1
         words = sql_text.replace(";", " ;").split()
 
+        
+    
         for i, word in enumerate(words, start=1):
-            if self.KEYWORDS_REGEX.fullmatch(word):
-                tokens.append(Token.Token("KEYWORD", word.upper(), line, i))
-            elif self.OPERATORS_REGEX.fullmatch(word):
-                tokens.append(Token.Token("OPERATOR", word, line, i))
-            elif self.SYMBOLS_REGEX.fullmatch(word):
-                tokens.append(Token.Token("SYMBOL", word, line, i))
-            elif self.WILDCARD_REGEX.fullmatch(word):
-                tokens.append(Token.Token("WILDCARD", word, line, i))
-            elif self.VALUE_REGEX.fullmatch(word):
-                tokens.append(Token.Token("VALUE", word, line, i))
-            elif self.IDENTIFIER_REGEX.fullmatch(word):
-                tokens.append(Token.Token("IDENTIFIER", word, line, i))
+            subwords = self.split_tokens(word)
+            for sub in subwords:
+                if self.KEYWORDS_REGEX.fullmatch(sub):
+                    tokens.append(Token("KEYWORD", sub.upper(), line, i))
+                elif self.OPERATORS_REGEX.fullmatch(sub):
+                    tokens.append(Token("OPERATOR", sub, line, i))
+                elif self.SYMBOLS_REGEX.fullmatch(sub):
+                    tokens.append(Token("SYMBOL", sub, line, i))
+                elif self.WILDCARD_REGEX.fullmatch(sub):
+                    tokens.append(Token("WILDCARD", sub, line, i))
+                elif self.VALUE_REGEX.fullmatch(sub):
+                    tokens.append(Token("VALUE", sub, line, i))
+                elif self.IDENTIFIER_REGEX.fullmatch(sub):
+                    tokens.append(Token("IDENTIFIER", sub, line, i))
+
             
 
-        tokens.append(Token.Token("EOF", None, line, len(words) + 1))
+        tokens.append(Token("EOF", None, line, len(words) + 1))
         return tokens
 
    
-# if __name__ == "__main__":
-#     sql="SELECT user FROM table_name WHERE id=5;"
-#     lexer = Lexer()
-#     for token in lexer.tokenize(sql):
-#         print(token)
+if __name__ == "__main__":
+    sql="SELECT user FROM table_name WHERE id=5;"
+    lexer = Lexer()
+    for token in lexer.tokenize(sql):
+        print(token)
 
 
--- Parser.py
+++ Parser.py
@@ -1,3 +1,6 @@
+from Lexer import *
+from ASTNode import *
+
 class Parser:
     def __init__(self, tokens): 
         self.tokens = tokens 
@@ -22,3 +25,128 @@ class Parser:
         elif kw == "DELETE":
              return self.parse_delete() 
         raise Exception(f"Syntax Error: Unknown start of query at {token.line}:{token.column}")
+
+
+    # Parser for SELECT
+    def parse_select(self):
+        select_list = []
+        if self.current_token().value.upper() != "SELECT": 
+            raise Exception("Syntax Error: Expected SELECT.\nAt line {token.line}, column {token.column} ") 
+        self.advance()
+
+        while True:
+            token = self.current_token()
+
+            if token.type == "WILDCARD":
+                select_list.append(ASTNode("WILDCARD", token.value)) 
+                self.advance()
+            elif token.type == "IDENTIFIER": 
+                select_list.append(ASTNode("COLUMN", token.value)) 
+                self.advance()
+            elif token.type == "KEYWORD": # Explicitly catch reserved keyword misuse 
+                raise Exception(f"Syntax Error: Reserved keyword '{token.value}' cannot be used as a column name.\nAt line {token.line}, column {token.column} ")    
+            else: 
+                break 
+            if self.current_token().type == "SYMBOL" and self.current_token().value == ",": 
+                self.advance() 
+                continue 
+            else: 
+                break
+
+        if self.current_token().value.upper() != "FROM": 
+            raise Exception(f"Syntax Error: Expected FROM.\nAt line {token.line}, column {token.column} ") 
+        self.advance()
+
+        table = self.current_token()
+        # nested query parsing
+        if table.type == "SYMBOL" and table.value == "(":
+            self.advance()
+            subquery = self.parse()  
+            if self.current_token().value != ")":
+                raise Exception(f"Syntax Error: Expected closing ) for subquery.\nAt line {token.line}, column {token.column} ")
+            self.advance()
+
+            children = [ASTNode("SELECT_LIST", children=select_list),
+                        ASTNode("SUBQUERY", children=[subquery])]
+            
+        else:
+            if table.type == "KEYWORD":
+                raise Exception(f"Syntax Error: Reserved keyword '{table.value}' cannot be used as a table name.\nAt line {token.line}, column {token.column} ")
+            if table.type != "IDENTIFIER":
+                raise Exception(f"Syntax Error: Expected table name.\nAt line {token.line}, column {token.column} ")
+            self.advance()
+
+            children = [ASTNode("SELECT_LIST", children=select_list),
+                        ASTNode("TABLE", table.value)]
+            
+        if self.current_token().type == "KEYWORD" and self.current_token().value.upper() == "WHERE":
+            self.advance()
+            condition = self.parse_condition()
+            children.append(ASTNode("WHERE", children=[condition]))
+        return ASTNode("SELECT_STMT", children=children)
+    
+
+    #Parser for Conditions
+    def parse_condition(self):
+        left = self.current_token()
+        if left.type == "KEYWORD":
+            raise Exception(f"Syntax Error: Reserved keyword '{left.value}' cannot be used as a column in condition.\nAt line {token.line}, column {token.column} ")
+        if left.type != "IDENTIFIER":
+            raise Exception(f"Syntax Error: Expected column in condition.\nAt line {token.line}, column {token.column} ")
+        self.advance()
+
+        op = self.current_token()
+        if op.type != "OPERATOR":
+            raise Exception(f"Syntax Error: Expected operator in condition.\nAt line {token.line}, column {token.column} ")
+        self.advance()
+
+        right = self.current_token()
+        if right.type == "VALUE":
+            self.advance()
+            return ASTNode("CONDITION", children=[
+                ASTNode("COLUMN", left.value),
+                ASTNode("OPERATOR", op.value),
+                ASTNode("VALUE", right.value)
+            ])
+        
+        elif right.type == "SYMBOL" and right.value == "(":
+            # Nested subquery in condition
+            self.advance()
+            subquery = self.parse()
+            if self.current_token().value != ")":
+                raise Exception(f"Syntax Error: Expected closing ) for subquery in condition.\nAt line {token.line}, column {token.column} ")
+            self.advance()
+            return ASTNode("CONDITION", children=[
+                ASTNode("COLUMN", left.value),
+                ASTNode("OPERATOR", op.value),
+                ASTNode("SUBQUERY", children=[subquery])
+            ])
+        
+        else:
+            raise Exception(f"Syntax Error: Expected value or subquery in condition.\nAt line {token.line}, column {token.column} ")
+    
+    #Parser for insert
+    def parse_insert(self):
+        pass
+
+    #Parser for update
+    def parse_update():
+        pass
+    
+    #Parser for delete
+    def parse_delete():
+        pass
+
+    
+
+
+if __name__ == "__main__": 
+    try:
+        lexer = Lexer() 
+        sql = "SELECT  name, age FROM users WHERE age=(select user from table1);" 
+        tokens = lexer.tokenize(sql) 
+        parser = Parser(tokens) 
+        ast = parser.parse() 
+        print(ast)
+    except Exception as e:
+        print(e)
